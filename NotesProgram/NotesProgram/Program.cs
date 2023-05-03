9b5a8ca484367eb1012fd26236ef0b630473c109
anuvr21 <129931334+anuvr21@users.noreply.github.com>
2023-05-04 00:39:03 +0530

    Add files via upload

---
+++ NotesProgram/NotesProgram/Program.cs
@@ -0,0 +1,46 @@
+﻿//write a program to enter an amount and find out nuumber of notes required to make that amount.
+using System;
+
+namespace NotesProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, n3, n4, n5, n6, n7, n8;
+            int amount;
+            Console.WriteLine("Enter the amount\n");
+            amount=int.Parse(Console.ReadLine());
+           
+            while(amount > 0) 
+            {
+                n1 = amount / 2000;
+                amount = amount % 2000;
+                n2 = amount / 500;
+                amount = amount % 500;
+                n3 = amount / 200;
+                amount = amount % 200;
+                n4 = amount / 100;
+                amount = amount % 100;
+                n5 = amount / 50;
+                amount = amount % 50;
+                n6 = amount / 20;
+                amount = amount % 20;
+                n7 = amount / 10;
+                amount = amount % 10;
+                n8 = amount / 5;
+                amount = amount % 5;
+
+                Console.WriteLine(n1 + "  2000");
+                Console.WriteLine(n2 + "  500");
+                Console.WriteLine(n3 + "  200");
+                Console.WriteLine(n4 + "  100");
+                Console.WriteLine(n5 + "  50");
+                Console.WriteLine(n6 + "  20");
+                Console.WriteLine(n7 + "  10");
+                Console.WriteLine(n8 + "  5");
+            }
+            
+        }
+    }
+}
